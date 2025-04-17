4e4337e931a023071114de4d44c6fd2ec41c0f88
AlistairPiot <alistair.piot@gmail.com>
2025-04-17 16:06:52 +0200

    ajout des plats dynamiquement + tests liste ustensils : pas encore OK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import logo from "./../assets/img/logo.svg";
 
 function Header() {
-    return (
-        <header className="container mt-4 mb-4">
-            <a href="./">
-                <div className="site-logo-container text-center">
-                    <img className="site-logo" alt="Site logo" src={logo} />
-                </div>
-                <h1 className="text-center site-title">Les grands plats</h1>
-            </a>
-        </header>
-    );
+  return (
+    <header className="container mt-4 mb-4">
+      <a href="./">
+        <div className="site-logo-container text-center">
+          <img className="site-logo" alt="Site logo" src={logo} />
+        </div>
+        <h1 className="text-center site-title">Les grands plats</h1>
+      </a>
+    </header>
+  );
 }
 
 export default Header;
