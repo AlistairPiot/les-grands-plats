0d41be894de2576e2d1d7aa1a8ed2cb4938a37aa
AlistairPiot <alistair.piot@gmail.com>
2025-04-18 09:54:32 +0200

    ustensils en cours

--- src/components/FilterBox.jsx
+++ src/components/FilterBox.jsx
@@ -1,76 +1,19 @@
 import React from "react";
-import imgDropdown from "./../assets/img/dropdown.svg";
-import { recipes } from "./../data/recipes"; // adapte le chemin selon ton projet
+import { recipes } from "./../data/recipes";
 
 function FilterBox() {
-  // Extraire les ustensiles uniques de toutes les recettes
-  const allUstensils = Array.from(
-    new Set(
-      recipes.flatMap((recipe) =>
-        recipe.ustensils.map((u) => u.toLowerCase().trim())
-      )
-    )
-  );
-
-  return (
-    <>
-      {/* ... autres dropdowns (Appliance, etc) ... */}
-
-      <div className="dropdown mb-3 me-md-3">
-        <div
-          className="btn btn-danger btn-lg dropdown-toggle btn-ustensils"
-          id="dropdownUstensils"
-          data-bs-toggle="dropdown"
-          data-bs-offset="0,0"
-        >
-          <div className="vertical-center">
-            <span className="btn-tag-title">Ustensiles</span>
-            <img
-              src={imgDropdown}
-              alt=""
-              className="ms-3 dropdown-arrow"
-              aria-hidden="true"
-            />
-            <form
-              id="form-ustensil"
-              role="search"
-              autoComplete="off"
-              className="hide"
-            >
-              <label className="visually-hidden" htmlFor="search-ustensil">
-                Ustensil Tag Search
-              </label>
-              <input
-                id="search-ustensil"
-                className="search-input col-12 rounded"
-                type="text"
-                placeholder="Rechercher un ustensile"
-              />
-            </form>
-          </div>
-        </div>
-
-        <div
-          className="dropdown-menu ustensils-dropdown rounded-0 rounded-bottom"
-          aria-labelledby="dropdownUstensils"
-        >
-          <ul id="ustensils-list">
-            {allUstensils.map((ustensil, index) => (
-              <li key={index} className="tags-ustensils">
-                <button
-                  className="dropdown-item btn-tag-ustensil"
-                  type="button"
-                  value={ustensil}
-                >
-                  {ustensil.charAt(0).toUpperCase() + ustensil.slice(1)}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h2>Ustensils</h2>
+            {recipes.map((recipe) => {
+                <div key={recipe.id}>
+                    {recipe.ustensils.map((ustensil, index) => {
+                        return <p key={index}>{ustensil}</p>;
+                    })}
+                </div>;
+            })}
+        </>
+    );
 }
 
 export default FilterBox;
